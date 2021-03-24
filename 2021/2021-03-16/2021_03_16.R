607c197eb575e177e8d835dde3e8604bac03cda2
Tuo Wang <tuowang@Tuos-Laptop.attlocal.net>
2021-03-24 17:35:12 -0500

    minor fix

--- 2021/2021-03-16/2021_03_16.R
+++ 2021/2021-03-16/2021_03_16.R
@@ -1,34 +1,32 @@
 # Title: TidyTuesday, 2021-02-23
 # Description: Blacks in the labor force
 # Author: Tuo Wang
-# Date: 2021-02-23
+# Date: 2021-03-23
 
 library(tidyverse)
-library(janitor)
-library(skimr)
-library(lubridate)
-library(showtext)
-library(ggtext)
-library(here)
-library(ggrepel)
+library(here)         # constructs
+library(skimr)        # skim the data
+library(lubridate)    # deal with date object
+library(showtext)     # download fonts
+library(ggrepel)      # automatically position non-overlapping text labels 
 
-font_add_google("Lato", "Lato")
-font_add_google("Merriweather", "Merriweather")
-font_add_google("Gochi Hand", "gochi")
-font_add_google("Chango", "chango")
-font_add_google("Lobster", "lobster")
-font_add_google("Bangers", "banger")
-font_add_google("Luckiest Guy", "luckiest")
-font_add_google("Roboto Slab", "roboto")
-font_add_google("Poppins", "poppins")
-showtext_auto()
+# font_add_google("Lato", "Lato")
+# font_add_google("Merriweather", "Merriweather")
+# font_add_google("Gochi Hand", "gochi")
+# font_add_google("Chango", "chango")
+# font_add_google("Lobster", "lobster")
+# font_add_google("Bangers", "banger")
+# font_add_google("Roboto Slab", "roboto")
+showtext::font_add_google("Luckiest Guy", "luckiest")
+showtext::font_add_google("Poppins", "poppins")
+showtext::showtext_auto()
 
+# Read Data
 tuesdata <- tidytuesdayR::tt_load('2021-03-16')
-
 game <- tuesdata$games
 
 # skimr::skim(game)
-# 
+
 # game %>%
 #   group_by(year, month) %>%
 #   summarise(n = n()) %>%
@@ -40,13 +38,11 @@ game <- tuesdata$games
 #   summarise(n = n_distinct(month)) %>%
 #   arrange(desc(year)) 
 
-game %>% 
-  filter(gamename== "PLAYERUNKNOWN'S BATTLEGROUNDS") %>%
-  View()
-  
+# game %>% 
+#   filter(gamename== "PLAYERUNKNOWN'S BATTLEGROUNDS") %>%
+#   View()
 
-# 1. 
-
+# Construct new variables for better visualization
 df <- game %>% 
   group_by(year, month) %>%
   summarise(avg_all_game = mean(avg),
@@ -75,29 +71,28 @@ df <- game %>%
 df %>%
   ggplot() +
   geom_line(aes(x=date, y=avg_all_game),size=1.5, color="#575757") + 
-  geom_line(aes(x=date, y=peak_all_game),size=1.5,color="#82786f")+
+  geom_line(aes(x=date, y=peak_all_game),size=1.5,color="#82786f") +
   annotate("text", x = ymd("2012-07-15"), y = 2500, fontface="bold",
-           label = "Average",family="Merriweather",color="#575757") +
+           label = "Average",family="poppins",color="#575757") +
   annotate("text", x = ymd("2012-07-15"), y = 4500, fontface="bold",
-           label = "Peak",family="Merriweather",color="#82786f") +
-  geom_line(data=filter(df, pandemic=='yes'),
-            aes(x=date, y=avg_all_game),size=1.5, color="#fa5555")+
-  geom_line(data=filter(df, pandemic=='yes'),
-            aes(x=date, y=peak_all_game),size=1.5, color="#fa5555")+
-  geom_point(data = filter(df, highlight==1),
-             aes(x=date, y=avg_all_game),size=3, shape=21,fill="#fa5555",color="white")+
-  geom_point(data = filter(df, highlight==1),
-             aes(x=date, y=peak_all_game),size=3, shape=21,fill="#fa5555",color="white")+
-  geom_label_repel(data = filter(df, highlight==1),hjust=1.3,vjust=-2,
-             aes(x=date, y=peak_all_game,label=label),family = "poppins")+
+           label = "Peak",family="poppins",color="#82786f") +
+  geom_line(data=filter(df, pandemic=='yes'),aes(x=date, y=avg_all_game),
+            size=1.5, color="#fa5555") +
+  geom_line(data=filter(df, pandemic=='yes'),aes(x=date, y=peak_all_game),
+            size=1.5, color="#fa5555") +
+  geom_point(data = filter(df, highlight==1),aes(x=date, y=avg_all_game),
+             size=3, shape=21,fill="#fa5555",color="white") +
+  geom_point(data = filter(df, highlight==1),aes(x=date, y=peak_all_game),
+             size=3, shape=21,fill="#fa5555",color="white") +
+  ggrepel::geom_label_repel(data = filter(df, highlight==1),hjust=1.3,vjust=-2,
+             aes(x=date, y=peak_all_game,label=label),family = "poppins") +
   labs(title="Steam Gaming Statistics from 2012 to 2021",
        subtitle = "Highest and average number of players at the same time",
-       x=NULL, y=NULL,
-       caption = "Data: TidyTuesday") +
-  scale_x_date(date_breaks = "2 year",date_labels = "%Y")+
+       x=NULL, y=NULL,caption = "Data: TidyTuesday") +
+  scale_x_date(date_breaks = "2 year",date_labels = "%Y") +
   theme(
     text = element_text(family = "luckiest", size = 8, color = "black"),
-    plot.title = element_markdown(family = "luckiest", size = 20,
+    plot.title = element_text(family = "luckiest", size = 20,
                                   face = "bold", color="#2a475e"),
     plot.subtitle = element_text(family = "poppins", size = 15, 
                                  face = "bold", color="#1b2838"),
