0c7b1e42ac14e23e0ae08f5ae4fbc3ec73e1ad57
Tuo Wang <tuowang@Tuos-Laptop.attlocal.net>
2021-04-21 09:50:04 -0500

    Apr 21

--- 2021/2021-04-20/2021-04-20.R
+++ 2021/2021-04-20/2021-04-20.R
@@ -90,15 +90,20 @@ all_stat %>%
     data = title_df2,
     aes(x=x,y=y,label=label),
     fill = NA, label.color = NA, family='roboto') +
+  geom_richtext(
+    aes(x=2020.5,y=7.5,label="Log of total titles"),color='white',
+    fill = NA, label.color = NA, family='roboto'
+  ) +
   scale_fill_manual(values = col_pal) +
   scale_color_manual(values = col_pal) +
   geom_point(aes(x=added_year, y=log(cum_year)),
              shape=21, fill=NA,color="grey25",size=16,
              stroke = 1.2)+
   scale_x_continuous(breaks = c(2008:2021)) +
-  scale_y_continuous(expand = c(0.05,0))+
+  scale_y_continuous(expand = c(0.05,0), position = 'right')+
   coord_cartesian(clip = 'off') +
-  labs(caption = "*Visualiation: Tuo Wang | Data Source: Kaggle*") +
+  labs(
+    caption = "*Visualiation: Tuo Wang | Data Source: Kaggle*") +
   theme_bw()+
   theme(
     text = element_text(family = "roboto", size = 10, color="white"),
@@ -116,8 +121,11 @@ all_stat %>%
     axis.text = element_text(size = 10, color = "white"),
     axis.title = element_text(size=12),
     axis.title.y = element_blank(),
-    axis.text.y = element_blank(),
-    axis.line.y = element_blank(),
+    axis.text.y = element_text(),
+    axis.line.y = element_line(
+      arrow = arrow(angle=30,length = unit(0.1, "inches"),type="closed" ),
+      size=2, 
+      color = "grey70"),
     axis.title.x = element_blank(),
     axis.ticks.y = element_blank(),
     axis.ticks.x = element_blank(),
